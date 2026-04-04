5cd8c1a4ff869099660ebad9268839dd0ebbb05f
Azmain Sheikh <123837974+azmainofficial@users.noreply.github.com>
2026-04-04 23:38:20 +0600

    Add derivation functions in sample_v2.cpp
    Implement leftmost and rightmost derivation functions.

---
+++ sample_v2.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+
+void LeftmostDerivation(char str[], int size){
+    char virtual_str[size];
+    strcpy(virtual_str, str);
+    printf("LMD :\n");
+    int counter = 0;
+    Jump:
+    printf("Step %d-> %s\n",++counter, virtual_str);
+    for(int i=0; i<size ; i++){
+        if(virtual_str[i] == 'A'){
+            virtual_str[i]='b';
+            goto Jump;
+        }else if(virtual_str[i]== 'B'){
+            virtual_str[i] = 'c';
+            goto Jump;
+        }
+
+    }
+}
+void RightmostDerivation(char str[], int size){
+    printf("RMD :\n");
+    int counter = 0;
+    Jump:
+    printf("Step %d-> %s\n",++counter, str);
+    for(int i=size-1; i >=0; i--){
+        if(str[i] == 'A'){
+            str[i]='b';
+            goto Jump;
+        }else if(str[i]== 'B'){
+            str[i] = 'c';
+            goto Jump;
+        }
+    }
+}
+
+int main(){
+    char str[] = {};
+
+    printf("Enter The Context Free Grammer S -> ");
+    scanf("%s",&str);
+    LeftmostDerivation(str, strlen(str));
+    RightmostDerivation(str, strlen(str));
+    free(str);
+    return 0;
+}
